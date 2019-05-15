8b345b1cf496629fe2ac00a7251a73b3dd709c40
Álisson R. Teles <alisson.teles@totvs.com.br>
2019-05-15 19:21:39 -0300

    Revisão 15 de Maio - Classificador

--- fbsecurity/src/sa/Classificador.java
+++ fbsecurity/src/sa/Classificador.java
@@ -1,34 +1,51 @@
 package sa;
 
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.BayesNet;
+import weka.classifiers.bayes.NaiveBayes;
+import weka.classifiers.evaluation.NominalPrediction;
+import weka.classifiers.evaluation.Prediction;
 import weka.classifiers.functions.SMO;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class Classificador {
 	static void Classificador() throws Exception {
-		DataSource source = new DataSource("classes-exp1.arff");
+		
+		DataSource source = new DataSource("classes-exp.arff");
 		Instances data = source.getDataSet();
 		
+		DataSource teste = new DataSource("teste_class.arff");
+		Instances testes = teste.getDataSet();
+
 		data.setClass(data.attribute("@@class@@"));
 		
-		 SMO smo = new SMO();
-	     smo.buildClassifier(data);
-	     
-	     Evaluation evaluation = new Evaluation(data);
-	     
-	     evaluation.evaluateModel(smo, data);
-	     
-	     System.out.println(smo.toString());
-	     
-	     System.out.println(evaluation.toSummaryString());
-	     
-	     System.out.println(evaluation.numTruePositives(1));
-	     
+		testes.setClass(testes.attribute("@@class@@"));
+
+		SMO smo = new SMO();
+		smo.buildClassifier(data);
+
+	
+		Evaluation evaluation = new Evaluation(testes);
+
+		evaluation.evaluateModel(smo, testes);
+
+		
+		
+		for (int i = 0; i < evaluation.predictions().size(); i++) {
+			Prediction ev = evaluation.predictions().get(i);
+			
+			NominalPrediction np = (NominalPrediction) ev;
+			
+			System.out.println(ev.actual() + " " + ev.predicted());
+			
+		}
+
 	}
-	
-	
-	
+
+
+
 	public static void main(String[] args) {
 		try {
 			Classificador();
